9b365fe5193287b503af11bc968ecbcd1431d7e6
Ynt-dot <kirillbarinov74@gmail.com>
2026-03-04 18:00:18 +0300

    Bug fix (double email)
    Make it impossible to register two accounts with one email address

--- cartoons/forms.py
+++ cartoons/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -11,6 +12,12 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError('Пользователь с таким email уже существует.')
+        return email
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
